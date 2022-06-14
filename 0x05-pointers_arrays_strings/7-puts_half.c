50df2d6a681f41fc0232572d58d745a798c6c982
mz-jennytech <okosisijenifer@gmail.com>
2022-06-13 17:06:50 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half - function name
+ * @str: paretwr
+ */
+void puts_half(char *str)
+{
+	int i = 0, len = 0, j;
+
+	while (str[i++])
+		len++;
+
+	if ((len % 2) == 0)
+		j = len / 2;
+	else
+		j = (len + 1) / 2;
+	for (i = j; i < len; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
