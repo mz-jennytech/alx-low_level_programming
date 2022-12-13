e5297b4b21546676351d9c8941733a112a27a8f3
mz-jennytech <okosisijenifer@gmail.com>
2022-12-12 18:28:13 -0800

    libdynamic

---
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcpy - Copy paste string
+ * @dest: destination
+ * @src: source
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+
+	return (dest);
+}
